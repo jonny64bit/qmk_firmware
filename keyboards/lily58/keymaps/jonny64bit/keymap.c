71dda74381c0e57c08b4832975223727e61bf500
Jonathan Dent <jon@jdent.co.uk>
2023-10-29 20:57:34 +0000

    feat: 🎸 attempt two at custom mappings

--- keyboards/lily58/keymaps/jonny64bit/keymap.c
+++ keyboards/lily58/keymaps/jonny64bit/keymap.c
@@ -3,91 +3,60 @@
 
 enum layer_number {
   _QWERTY = 0,
-  _LOWER,
-  _RAISE,
+  _SYMBOLS,
   _MOVEMENT,
-  _EXTRA
+  _RAISE,
+  _LOWER
 };
-
-// Left-hand home row mods
-#define HOME_A LGUI_T(KC_A)
-#define HOME_S LALT_T(KC_S)
-#define HOME_D LCTL_T(KC_D)
-#define HOME_F LSFT_T(KC_F)
-
-// Right-hand home row mods
-#define HOME_J RSFT_T(KC_J)
-#define HOME_K RCTL_T(KC_K)
-#define HOME_L LALT_T(KC_L)
-#define HOME_SCLN RGUI_T(KC_SCLN)
 
 // Tap Dance declarations
 enum {
-    TD_ENT_ESC,
+    TD_J_ESC,
 };
 
 // Tap Dance definitions
 tap_dance_action_t tap_dance_actions[] = {
-    // Tap once for Enter, twice for Escape
-    [TD_ENT_ESC] = ACTION_TAP_DANCE_DOUBLE(KC_ENT, KC_ESC),
+    // Tap once for J, twice for Escape
+    [TD_J_ESC] = ACTION_TAP_DANCE_DOUBLE(KC_J, KC_ESC),
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-//    ┌──────┬────────┬────────┬────────┬─────────────┬─────────────┐                                         ┌──────┬────────┬─────────────┬────────┬───────────┬─────┐
-//    │ esc  │   1    │   2    │   3    │      4      │      5      │                                         │  6   │   7    │      8      │   9    │     0     │  `  │
-//    ├──────┼────────┼────────┼────────┼─────────────┼─────────────┤                                         ├──────┼────────┼─────────────┼────────┼───────────┼─────┤
-//    │ tab  │   q    │   w    │   e    │      r      │      t      │                                         │  y   │   u    │      i      │   o    │     p     │  -  │
-//    ├──────┼────────┼────────┼────────┼─────────────┼─────────────┤                                         ├──────┼────────┼─────────────┼────────┼───────────┼─────┤
-//    │ lctl │ HOME_A │ HOME_S │ HOME_D │   HOME_F    │      g      │                                         │  h   │ HOME_J │   HOME_K    │ HOME_L │ HOME_SCLN │  '  │
-//    ├──────┼────────┼────────┼────────┼─────────────┼─────────────┼────────────────────┐   ┌────────────────┼──────┼────────┼─────────────┼────────┼───────────┼─────┤
-//    │ lalt │   z    │   x    │   c    │      v      │      b      │         [          │   │       ]        │  n   │   m    │      ,      │   .    │     /     │ del │
-//    └──────┴────────┴────────┼────────┼─────────────┼─────────────┼────────────────────┤   ├────────────────┼──────┼────────┼─────────────┼────────┴───────────┴─────┘
-//                             │        │ OSL(_LOWER) │ OSL(_RAISE) │ LT(_MOVEMENT, spc) │   │ TD(TD_ENT_ESC) │ bspc │        │ OSL(_EXTRA) │
-//                             └────────┴─────────────┴─────────────┴────────────────────┘   └────────────────┴──────┴────────┴─────────────┘
+//    ┌─────────────────────┬───┬───┬─────────────┬─────────────┬───────────────┐                              ┌──────┬──────────────┬─────────┬───┬───┬───────────────┐
+//    │        pscr         │ 1 │ 2 │      3      │      4      │       5       │                              │  6   │      7       │    8    │ 9 │ 0 │       `       │
+//    ├─────────────────────┼───┼───┼─────────────┼─────────────┼───────────────┤                              ├──────┼──────────────┼─────────┼───┼───┼───────────────┤
+//    │ QK_CAPS_WORD_TOGGLE │ q │ w │      e      │      r      │       t       │                              │  y   │      u       │    i    │ o │ p │       -       │
+//    ├─────────────────────┼───┼───┼─────────────┼─────────────┼───────────────┤                              ├──────┼──────────────┼─────────┼───┼───┼───────────────┤
+//    │        lsft         │ a │ s │      d      │      f      │       g       │                              │  h   │ TD(TD_J_ESC) │    k    │ l │ ; │       '       │
+//    ├─────────────────────┼───┼───┼─────────────┼─────────────┼───────────────┼────────────────────┐   ┌─────┼──────┼──────────────┼─────────┼───┼───┼───────────────┤
+//    │        lctl         │ z │ x │      c      │      v      │       b       │         [          │   │  ]  │  n   │      m       │    ,    │ . │ / │ QK_REPEAT_KEY │
+//    └─────────────────────┴───┴───┼─────────────┼─────────────┼───────────────┼────────────────────┤   ├─────┼──────┼──────────────┼─────────┼───┴───┴───────────────┘
+//                                  │ OSL(_LOWER) │ OSL(_RAISE) │ OSL(_SYMBOLS) │ LT(_MOVEMENT, spc) │   │ ent │ bspc │     del      │ LGUI(.) │
+//                                  └─────────────┴─────────────┴───────────────┴────────────────────┘   └─────┴──────┴──────────────┴─────────┘
 [_QWERTY] = LAYOUT(
-  KC_ESC  , KC_1   , KC_2   , KC_3    , KC_4        , KC_5        ,                                                KC_6    , KC_7    , KC_8        , KC_9   , KC_0      , KC_GRV   ,
-  KC_TAB  , KC_Q   , KC_W   , KC_E    , KC_R        , KC_T        ,                                                KC_Y    , KC_U    , KC_I        , KC_O   , KC_P      , KC_MINS  ,
-  KC_LCTL , HOME_A , HOME_S , HOME_D  , HOME_F      , KC_G        ,                                                KC_H    , HOME_J  , HOME_K      , HOME_L , HOME_SCLN , KC_QUOT  ,
-  KC_LALT , KC_Z   , KC_X   , KC_C    , KC_V        , KC_B        , KC_LBRC                 ,     KC_RBRC        , KC_N    , KC_M    , KC_COMM     , KC_DOT , KC_SLSH   , KC_DELETE,
-                              _______ , OSL(_LOWER) , OSL(_RAISE) , LT(_MOVEMENT, KC_SPACE) ,     TD(TD_ENT_ESC) , KC_BSPC , _______ , OSL(_EXTRA)
+  KC_PSCR             , KC_1 , KC_2 , KC_3        , KC_4        , KC_5          ,                                         KC_6    , KC_7         , KC_8         , KC_9   , KC_0    , KC_GRV       ,
+  QK_CAPS_WORD_TOGGLE , KC_Q , KC_W , KC_E        , KC_R        , KC_T          ,                                         KC_Y    , KC_U         , KC_I         , KC_O   , KC_P    , KC_MINS      ,
+  KC_LSFT             , KC_A , KC_S , KC_D        , KC_F        , KC_G          ,                                         KC_H    , TD(TD_J_ESC) , KC_K         , KC_L   , KC_SCLN , KC_QUOT      ,
+  KC_LCTL             , KC_Z , KC_X , KC_C        , KC_V        , KC_B          , KC_LBRC                 ,     KC_RBRC , KC_N    , KC_M         , KC_COMM      , KC_DOT , KC_SLSH , QK_REPEAT_KEY,
+                                      OSL(_LOWER) , OSL(_RAISE) , OSL(_SYMBOLS) , LT(_MOVEMENT, KC_SPACE) ,     KC_ENT  , KC_BSPC , KC_DELETE    , LGUI(KC_DOT)
 ),
 
-//    ┌─────┬─────┬─────────┬────────┬─────┬─────┐               ┌─────────┬────────┬─────┬─────┬─────┬─────┐
-//    │     │     │         │        │     │     │               │         │        │     │     │     │     │
-//    ├─────┼─────┼─────────┼────────┼─────┼─────┤               ├─────────┼────────┼─────┼─────┼─────┼─────┤
-//    │ f1  │ f2  │   f3    │   f4   │ f5  │ f6  │               │   f7    │   f8   │ f9  │ f10 │ f11 │ f12 │
-//    ├─────┼─────┼─────────┼────────┼─────┼─────┤               ├─────────┼────────┼─────┼─────┼─────┼─────┤
-//    │  `  │  !  │ UK_DQUO │ UK_PND │  $  │  %  │               │    ^    │   &    │  *  │  (  │  )  │  ~  │
-//    ├─────┼─────┼─────────┼────────┼─────┼─────┼─────┐   ┌─────┼─────────┼────────┼─────┼─────┼─────┼─────┤
-//    │     │     │         │        │     │  \  │     │   │     │ UK_PIPE │ UK_EQL │  +  │  {  │  }  │  _  │
-//    └─────┴─────┴─────────┼────────┼─────┼─────┼─────┤   ├─────┼─────────┼────────┼─────┼─────┴─────┴─────┘
-//                          │        │     │     │     │   │     │         │        │     │
-//                          └────────┴─────┴─────┴─────┘   └─────┴─────────┴────────┴─────┘
-[_RAISE] = LAYOUT(
-  _______ , _______ , _______ , _______ , _______ , _______ ,                         _______ , _______ , _______ , _______ , _______ , _______,
-  KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   , KC_F6   ,                         KC_F7   , KC_F8   , KC_F9   , KC_F10  , KC_F11  , KC_F12 ,
-  KC_GRV  , KC_EXLM , UK_DQUO , UK_PND  , KC_DLR  , KC_PERC ,                         KC_CIRC , KC_AMPR , KC_ASTR , KC_LPRN , KC_RPRN , KC_TILD,
-  _______ , _______ , _______ , _______ , _______ , KC_BSLS , _______ ,     _______ , UK_PIPE , UK_EQL  , KC_PLUS , KC_LCBR , KC_RCBR , KC_UNDS,
-                                _______ , _______ , _______ , _______ ,     _______ , _______ , _______ , _______
-),
-
-//    ┌─────┬─────┬─────┬─────┬─────┬─────┐               ┌─────┬──────┬──────┬─────┬──────┬─────┐
-//    │     │     │     │     │     │     │               │     │      │      │     │      │     │
-//    ├─────┼─────┼─────┼─────┼─────┼─────┤               ├─────┼──────┼──────┼─────┼──────┼─────┤
-//    │  `  │  1  │  2  │  3  │  4  │  5  │               │  6  │  7   │  8   │  9  │  0   │     │
-//    ├─────┼─────┼─────┼─────┼─────┼─────┤               ├─────┼──────┼──────┼─────┼──────┼─────┤
-//    │ f1  │ f2  │ f3  │ f4  │ f5  │ f6  │               │ no  │ left │ down │ up  │ rght │ no  │
-//    ├─────┼─────┼─────┼─────┼─────┼─────┼─────┐   ┌─────┼─────┼──────┼──────┼─────┼──────┼─────┤
-//    │ f7  │ f8  │ f9  │ f10 │ f11 │ f12 │     │   │     │  +  │  -   │  =   │  [  │  ]   │  \  │
-//    └─────┴─────┴─────┼─────┼─────┼─────┼─────┤   ├─────┼─────┼──────┼──────┼─────┴──────┴─────┘
-//                      │     │     │     │     │   │     │     │      │      │
-//                      └─────┴─────┴─────┴─────┘   └─────┴─────┴──────┴──────┘
-[_LOWER] = LAYOUT(
-  _______ , _______ , _______ , _______ , _______ , _______ ,                         _______ , _______ , _______ , _______ , _______ , _______,
-  KC_GRV  , KC_1    , KC_2    , KC_3    , KC_4    , KC_5    ,                         KC_6    , KC_7    , KC_8    , KC_9    , KC_0    , _______,
-  KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   , KC_F6   ,                         XXXXXXX , KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT , XXXXXXX,
-  KC_F7   , KC_F8   , KC_F9   , KC_F10  , KC_F11  , KC_F12  , _______ ,     _______ , KC_PLUS , KC_MINS , KC_EQL  , KC_LBRC , KC_RBRC , KC_BSLS,
-                                _______ , _______ , _______ , _______ ,     _______ , _______ , _______ , _______
+//    ┌─────┬─────────┬─────────┬───────┬─────┬─────────┐               ┌─────────┬───────────────┬───────────────┬───────────────┬───────────────┬─────┐
+//    │     │         │         │       │     │         │               │         │               │               │               │               │     │
+//    ├─────┼─────────┼─────────┼───────┼─────┼─────────┤               ├─────────┼───────────────┼───────────────┼───────────────┼───────────────┼─────┤
+//    │     │ UK_PND  │    [    │   {   │  (  │ UK_TILD │               │    ^    │       )       │       }       │       ]       │       `       │     │
+//    ├─────┼─────────┼─────────┼───────┼─────┼─────────┤               ├─────────┼───────────────┼───────────────┼───────────────┼───────────────┼─────┤
+//    │     │    -    │    *    │   =   │  _  │    $    │               │ UK_HASH │ OSM(MOD_LSFT) │ OSM(MOD_LCTL) │ OSM(MOD_LALT) │ OSM(MOD_LGUI) │     │
+//    ├─────┼─────────┼─────────┼───────┼─────┼─────────┼─────┐   ┌─────┼─────────┼───────────────┼───────────────┼───────────────┼───────────────┼─────┤
+//    │     │ UK_PLUS │ UK_PIPE │ UK_AT │  /  │    %    │     │   │     │   no    │    UK_BSLS    │       &       │       ?       │       !       │     │
+//    └─────┴─────────┴─────────┼───────┼─────┼─────────┼─────┤   ├─────┼─────────┼───────────────┼───────────────┼───────────────┴───────────────┴─────┘
+//                              │       │     │         │     │   │     │         │               │               │
+//                              └───────┴─────┴─────────┴─────┘   └─────┴─────────┴───────────────┴───────────────┘
+[_SYMBOLS] = LAYOUT(
+  _______ , _______ , _______ , _______ , _______ , _______ ,                         _______ , _______       , _______       , _______       , _______       , _______,
+  _______ , UK_PND  , KC_LBRC , KC_LCBR , KC_LPRN , UK_TILD ,                         KC_CIRC , KC_RPRN       , KC_RCBR       , KC_RBRC       , KC_GRV        , _______,
+  _______ , KC_MINS , KC_ASTR , KC_EQL  , KC_UNDS , KC_DLR  ,                         UK_HASH , OSM(MOD_LSFT) , OSM(MOD_LCTL) , OSM(MOD_LALT) , OSM(MOD_LGUI) , _______,
+  _______ , UK_PLUS , UK_PIPE , UK_AT   , KC_SLSH , KC_PERC , _______ ,     _______ , XXXXXXX , UK_BSLS       , KC_AMPR       , KC_QUES       , KC_EXLM       , _______,
+                                _______ , _______ , _______ , _______ ,     _______ , _______ , _______       , _______
 ),
 
 //    ┌────┬────┬────┬─────┬─────┬─────┐               ┌──────┬──────┬──────┬──────┬─────┬─────┐
@@ -106,25 +75,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,                         KC_F7   , KC_F8   , KC_F9   , KC_F10  , KC_F11  , KC_F12 ,
   XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,                         KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT , XXXXXXX , XXXXXXX,
   XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,     XXXXXXX , KC_HOME , KC_PGDN , KC_PGUP , KC_END  , XXXXXXX , XXXXXXX,
-                                _______ , _______ , _______ , _______ ,     _______ , _______ , _______ , _______
-),
-
-//    ┌────┬────┬────┬─────┬─────┬─────┐               ┌─────┬─────┬─────┬────┬────┬────┐
-//    │ no │ no │ no │ no  │ no  │ no  │               │ no  │ no  │ no  │ no │ no │ no │
-//    ├────┼────┼────┼─────┼─────┼─────┤               ├─────┼─────┼─────┼────┼────┼────┤
-//    │ no │ no │ no │ no  │ no  │ no  │               │ no  │ no  │ no  │ no │ no │ no │
-//    ├────┼────┼────┼─────┼─────┼─────┤               ├─────┼─────┼─────┼────┼────┼────┤
-//    │ no │ no │ no │ no  │ no  │ no  │               │ no  │ no  │ no  │ no │ no │ no │
-//    ├────┼────┼────┼─────┼─────┼─────┼─────┐   ┌─────┼─────┼─────┼─────┼────┼────┼────┤
-//    │ no │ no │ no │ no  │ no  │ no  │ no  │   │ no  │ no  │ no  │ no  │ no │ no │ no │
-//    └────┴────┴────┼─────┼─────┼─────┼─────┤   ├─────┼─────┼─────┼─────┼────┴────┴────┘
-//                   │     │     │     │     │   │     │     │     │     │
-//                   └─────┴─────┴─────┴─────┘   └─────┴─────┴─────┴─────┘
-[_EXTRA] = LAYOUT(
-  XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,                         XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX,
-  XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,                         XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX,
-  XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,                         XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX,
-  XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,     XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX,
                                 _______ , _______ , _______ , _______ ,     _______ , _______ , _______ , _______
 )
 };
@@ -170,8 +120,8 @@ void render_default_layer_state(void) {
         case _MOVEMENT:
             oled_write_ln_P(PSTR("MOVE"), false);
             break;
-        case _EXTRA:
-            oled_write_ln_P(PSTR("EXTRA"), false);
+        case _SYMBOLS:
+            oled_write_ln_P(PSTR("SYMBO"), false);
             break;
         default:
             oled_write_ln_P(PSTR("Undefined"), false);
